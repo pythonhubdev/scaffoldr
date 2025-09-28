ed6406296f05b1a3da26e617b369d2d8e2a642f3
Vetrichelvan <pythonhubdev@gmail.com>
2025-09-28 13:10:05 +0530

    feat: add database support with async SQLAlchemy integration and session management

--- templates/fastapi_template/{{ project_name }}/src/{{ project_slug }}/api/lifespan.py
+++
@@ -1,9 +0,0 @@
-from collections.abc import AsyncGenerator
-from contextlib import asynccontextmanager
-
-from fastapi import FastAPI
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI) -> AsyncGenerator[None]:
-	yield
